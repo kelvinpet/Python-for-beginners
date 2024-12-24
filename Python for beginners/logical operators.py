9445685c1a430140fe18e9dd9c6574ffb3a58b67
Kelvin Peters <132689203+kelvinpet@users.noreply.github.com>
2024-12-24 14:18:10 +0100

    Add files via upload

---
+++ Python for beginners/logical operators.py
@@ -0,0 +1,13 @@
+price = 25
+print(price > 10 and price < 30)
+print(price > 10 or price < 30)
+price = 5
+print(price > 10 and price < 30)
+print(price > 10 or price < 30)
+price = 5
+print(not price > 10)
+
+logical operators available
+and (both)
+or (at least one)
+not 
